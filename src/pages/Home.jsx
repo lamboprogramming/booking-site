a96caf1f593e2e798e336f34071b3ced0e309083
Lamboprogramming <gillespielamar@gmail.com>
2023-01-26 23:09:02 -0500

    Hero info changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
      
      
     
-     <p className="text-xl italic leading-10 tracking-widest text-white uppercase shadow-md md:text-2xl lg:text-3xl md:mb-4">Kompa, Afrobeats, Bachata, Reggaeton, Denbow, Rap, Hip Hop, Soul, Reggae, Dancehall, Pop, Alternative, Funk & more... </p>
+     <p className="text-xl italic leading-10 tracking-widest text-white uppercase shadow-md md:text-2xl lg:text-3xl md:mb-4">Private Events, Weddings, Baby Showers, Corporate Events, Retreats, Grand Openings, Holiday Parties,Pop-up Shops, Gaming Events, & More !  </p>
       
      <Link to="/contact">
      <button className="lg:w-44 px-4 py-[.5%] text-2xl font-bold text-white border-2 rounded-md bg-gradient-to-r from-fuchsia-700 to-indigo-500 hover:from-indigo-500 hover:to-fuchsia-500 sm:w-[20] mr-1 hover:rounded-3xl duration-500 nav-links tracking-widest mt-3 md:mt-0">Contact</button>
