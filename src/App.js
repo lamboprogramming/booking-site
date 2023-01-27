70f82a82c95426ed725c43455d8077d9db429f7b
Lamboprogramming <gillespielamar@gmail.com>
2023-01-26 21:30:01 -0500

    navbar implemented and working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Navbar from './pages/Navbar'
+import Music from './pages/Music'
+import Media from './pages/Media'
+import Contact from './pages/Contact'
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="media" element={<Media />} />
+      <Route path="music" element={<Music />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
